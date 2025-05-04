76d9e55af2cf9b7049c80e44035f78bf110fac89
AsahiKobayashi <k0b4y4s81@icloud.com>
2025-05-04 16:20:41 +0900

    ロー法

---
+++ lib/math/Pollard.java
@@ -0,0 +1,81 @@
+package lib.math;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Pollard's Rho 法 : ポラードのロー法
+ * 
+ * 64bit整数以下の任意の値で素因数分解を行います
+ */
+class Pollard {
+
+        private final BigInteger ZERO = new BigInteger("0");
+        private final BigInteger ONE  = new BigInteger("1");
+        private final BigInteger TWO  = new BigInteger("2");
+        private final java.security.SecureRandom random = new java.security.SecureRandom();
+
+        /**
+         * 素因数分解を行います
+         * @order N^(1/4) = 4乗根
+         * @param n
+         * @return
+         */
+        public final Map<Long, Integer> fact (long n) {
+                Map<Long, Integer> map = new HashMap<>();
+                factor(new BigInteger(String.valueOf(n)), map);
+                return map;
+        }
+
+        private BigInteger f (BigInteger x, BigInteger c, BigInteger N) {
+                return x.multiply(x).mod(N).add(c);
+        }
+
+        private BigInteger[] smallPrimes = {
+                new BigInteger("2")
+        };
+
+        private BigInteger smallPrimeDivisor(BigInteger N) {
+                for(BigInteger p : smallPrimes) {
+                if (N.mod(p) == ZERO)
+                        return p;
+                }
+                return ZERO;
+        }
+
+        private BigInteger divisor(BigInteger N) {
+                BigInteger c = new BigInteger(N.bitLength(), random);
+                BigInteger x = new BigInteger(N.bitLength(), random);
+                BigInteger y = x;
+                BigInteger divisor = ONE;
+                if (N.mod(TWO).compareTo(ZERO) == 0) return TWO;
+                while((divisor.compareTo(ONE)) == 0) {
+                        x = f(x,c,N);
+                        y = f(f(y,c,N),c,N);
+                        divisor = x.subtract(y).gcd(N);
+                }
+                
+                return divisor;
+        }
+
+        private final void factor(BigInteger N, Map<Long, Integer> map) {
+                if (N.compareTo(ONE) == 0) return;
+                if (N.isProbablePrime(10)) {
+                        long n = Long.parseLong(N.toString());
+                        if (map.get(n) == null) 
+                                map.put(n, 1);
+                        else 
+                                map.put(n, map.get(n) + 1);
+                        return; 
+                }
+                if (N.bitLength() > 74) {
+                        return;
+                }
+                BigInteger p = smallPrimeDivisor(N);
+                BigInteger d = (p != ZERO) ? p : divisor(N);
+                factor(d, map);
+                factor(N.divide(d), map);
+        }
+
+    }
